44bb1b20f5130434353e62dd812fb9f9891c83c3
Ethan-Prieur1 <99287118+Ethan-Prieur1@users.noreply.github.com>
2022-10-06 09:02:17 -0400

    Update stack.ts

--- src/stack.ts
+++ src/stack.ts
@@ -1,1 +1,21 @@
-test
+/**
+ * This class creates a stack
+ *
+ * By:      Ethan Prieur
+ * Version: 1.0
+ * Since:   2022-10-04
+ */
+
+class Stack {
+  stackArray: number[] = []
+
+  push(pushedNumber: number): void {
+    // add a number to the array
+    this.stackArray.push(pushedNumber)
+  }
+
+  showStack(): void {
+    console.log(this.stackArray)
+  }
+}
+export = Stack
